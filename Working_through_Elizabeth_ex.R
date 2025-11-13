12e67557b622592a750682a02f530edd5005602e
kmschoenecker <kschoenecker@wisc.edu>
2025-11-13 14:38:17 -0600

    Working on Elizabeth's example problem

---
+++ Working_through_Elizabeth_ex.R
@@ -0,0 +1,5 @@
+#### Working through Elizabeth's example data ----
+
+# Ecological context: King Pendragon is trying to determine which dragons to hunt.
+# Red dragons or white dragons, our just hunt based on size
+# he needs to know so he can limit damage to the kingdom (acres burned)
